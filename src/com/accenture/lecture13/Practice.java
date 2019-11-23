4390c0879c25e21f441f8072c10b7ad3e0a08006
student <olgaru@gmail.com@gmail.com>
2019-11-23 10:26:02 +0200

    first part

---
+++ src/com/accenture/lecture13/Practice.java
@@ -0,0 +1,13 @@
+package com.accenture.lecture13;
+
+public class Practice {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
